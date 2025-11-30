4d1addeb6290e354c24f188c4612cd10f0901ff5
Miguel Villarraga Franco <143026348+Miguelvillarraga@users.noreply.github.com>
2025-11-30 15:03:45 -0500

    Update pregunta_12.py

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_12():
     """
@@ -22,3 +23,9 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    df = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    df = df.sort_values(["c0", "c5a"])
+    df["pair"] = df["c5a"] + ":" + df["c5b"].astype(str)
+    resultado = df.groupby("c0")["pair"].apply(lambda x: ",".join(x)).reset_index(name="c5")
+
+    return resultado
